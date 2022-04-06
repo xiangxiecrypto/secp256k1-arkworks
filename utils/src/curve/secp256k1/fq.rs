01c0c8c6c4e41a16bca0a0302392e57860d51f4f
wtd2 <zhangwh2018@mail.sustech.edu.cn>
2022-04-06 23:43:15 +0800

    parameters adjusted

--- utils/src/curve/secp256k1/fq.rs
+++ utils/src/curve/secp256k1/fq.rs
@@ -1,38 +1,40 @@
-use ark_ff::{biginteger::BigInteger256 as BigInteger, FftParameters, Fp256, Fp256Parameters};
-pub type Fq = Fp256<FpParameters>;
+use ark_ff::{biginteger::BigInteger320 as BigInteger, FftParameters, Fp320, Fp320Parameters};
+pub type Fq = Fp320<FpParameters>;
 
 pub struct FpParameters;
 
-impl Fp256Parameters for FpParameters {}
+impl Fp320Parameters for FpParameters {}
 impl FftParameters for FpParameters {
     type BigInt = BigInteger;
 
     const TWO_ADICITY: u32 = 1;
 
     #[rustfmt::skip]
-    //115792089237316195423570985008687907853269984665640564039457584007908834671662
+    //115792089237316195423570985008687907853269984665561335858920850710301058792495
     const TWO_ADIC_ROOT_OF_UNITY: BigInteger = BigInteger([
-        0xFFFFFFFEFFFFFC2E,
-        0xFFFFFFFFFFFFFFFF,
-        0xFFFFFFFFFFFFFFFF,
-        0xFFFFFFFFFFFFFFFF,
-        ]);
+        0xfffffffefffffc2f,
+        0xfffffffefffffc2e,
+        0xffffffffffffffff,
+        0xffffffffffffffff,
+        0x0000000000000000
+    ]);
 }
 
 impl ark_ff::FpParameters for FpParameters {
     //115792089237316195423570985008687907853269984665640564039457584007908834671663
     const MODULUS: BigInteger = BigInteger([
-        0xFFFFFFFEFFFFFC2F,
-        0xFFFFFFFFFFFFFFFF,
-        0xFFFFFFFFFFFFFFFF,
-        0xFFFFFFFFFFFFFFFF,
+        0xfffffffefffffc2f,
+        0xffffffffffffffff,
+        0xffffffffffffffff,
+        0xffffffffffffffff,
+        0x0000000000000000
     ]);
 
-    //4294968273
-    const R: BigInteger = BigInteger([0x1000003D1, 0x0, 0x0, 0x0]);
+    //4294968273 * 2^64
+    const R: BigInteger = BigInteger([0x0, 0x1000003D1, 0x0, 0x0, 0x0]);
 
-    //18446752466076602529
-    const R2: BigInteger = BigInteger([0x000007A2000E90A1, 0x1, 0x0, 0x0]);
+    //18446752466076602529 * 2^128
+    const R2: BigInteger = BigInteger([0x0, 0x0, 0x000007A2000E90A1, 0x1, 0x0]);
 
     //57896044618658097711785492504343953926634992332820282019728792003954417335831
     //7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF7FFFFE17
@@ -41,6 +43,7 @@ impl ark_ff::FpParameters for FpParameters {
         0xFFFFFFFFFFFFFFFF,
         0xFFFFFFFFFFFFFFFF,
         0x7FFFFFFFFFFFFFFF,
+        0x0,
     ]);
 
     //57896044618658097711785492504343953926634992332820282019728792003954417335831
@@ -50,6 +53,7 @@ impl ark_ff::FpParameters for FpParameters {
         0xFFFFFFFFFFFFFFFF,
         0xFFFFFFFFFFFFFFFF,
         0x7FFFFFFFFFFFFFFF,
+        0x0,
     ]);
 
     //28948022309329048855892746252171976963317496166410141009864396001977208667915
@@ -59,16 +63,23 @@ impl ark_ff::FpParameters for FpParameters {
         0xFFFFFFFFFFFFFFFF,
         0xFFFFFFFFFFFFFFFF,
         0x3FFFFFFFFFFFFFFF,
+        0x0,
     ]);
 
     //GENERATOR = 3
-    const GENERATOR: BigInteger = BigInteger([0x3, 0x0, 0x0, 0x0]);
+    const GENERATOR: BigInteger = BigInteger([
+        0x0000000000000000,
+        0x0000000300000b73,
+        0x0000000000000000,
+        0x0000000000000000,
+        0x0000000000000000
+    ]);
 
     const MODULUS_BITS: u32 = 256;
 
     const CAPACITY: u32 = Self::MODULUS_BITS - 1;
 
-    const REPR_SHAVE_BITS: u32 = 0;
+    const REPR_SHAVE_BITS: u32 = 64;
 
     const INV: u64 = 15580212934572586289;
 }
--- utils/src/curve/secp256k1/fr.rs
+++ utils/src/curve/secp256k1/fr.rs
@@ -1,43 +1,45 @@
-use ark_ff::{biginteger::BigInteger256 as BigInteger, FftParameters, Fp256, Fp256Parameters};
-pub type Fr = Fp256<FpParameters>;
+use ark_ff::{biginteger::BigInteger320 as BigInteger, FftParameters, Fp320, Fp320Parameters};
+pub type Fr = Fp320<FpParameters>;
 
 pub struct FpParameters;
 
-impl Fp256Parameters for FpParameters {}
+impl Fp320Parameters for FpParameters {}
 impl FftParameters for FpParameters {
     type BigInt = BigInteger;
 
     const TWO_ADICITY: u32 = 6;
 
     #[rustfmt::skip]
-    //5480320495727936603795231718619559942670027629901634955707709633242980176626
-    //C1DC060E7A91986DF9879A3FBC483A898BDEAB680756045992F4B5402B052F2
+    //38715229011166149576104664426121941072306984375677460650018045533266598568266
     const TWO_ADIC_ROOT_OF_UNITY: BigInteger = BigInteger([
-        0x992F4B5402B052F2,
-        0x98BDEAB680756045,
-        0xDF9879A3FBC483A8,
-        0xC1DC060E7A91986,
-        ]);
+        0x0112cb0f605a214a,
+        0x92225daffb794500,
+        0x7e42003a6ccb6212,
+        0x55980b07bc222114,
+        0x0000000000000000
+    ]);
 }
 
 impl ark_ff::FpParameters for FpParameters {
     //115792089237316195423570985008687907852837564279074904382605163141518161494337
     const MODULUS: BigInteger = BigInteger([
-        0xBFD25E8CD0364141,
-        0xBAAEDCE6AF48A03B,
-        0xFFFFFFFFFFFFFFFE,
-        0xFFFFFFFFFFFFFFFF,
+        0xbfd25e8cd0364141,
+        0xbaaedce6af48a03b,
+        0xfffffffffffffffe,
+        0xffffffffffffffff,
+        0x0000000000000000
     ]);
 
-    //432420386565659656852420866394968145599
-    const R: BigInteger = BigInteger([0x402DA1732FC9BEBF, 0x4551231950B75FC4, 0x1, 0x0]);
+    //432420386565659656852420866394968145599 * 2^64
+    const R: BigInteger = BigInteger([0x0, 0x402DA1732FC9BEBF, 0x4551231950B75FC4, 0x1, 0x0]);
 
-    //71195301480278335217902614543643724933430614355449737089222010364394701574464
+    //27185382341430777127667013160941942500583767731514336557959633868809832333177
     const R2: BigInteger = BigInteger([
-        0x896CF21467D7D140,
-        0x741496C20E7CF878,
-        0xE697F5E45BCD07C6,
-        0x9D671CD581C69BC5,
+        0x1e004f504dfd7f79,
+        0x08fcf59774a052ea,
+        0x27c4120fc94e1653,
+        0x3c1a6191e5702644,
+        0x0000000000000000
     ]);
 
     //57896044618658097711785492504343953926418782139537452191302581570759080747168
@@ -47,6 +49,7 @@ impl ark_ff::FpParameters for FpParameters {
         0x5D576E7357A4501D,
         0xFFFFFFFFFFFFFFFF,
         0x7FFFFFFFFFFFFFFF,
+        0x0
     ]);
 
     //1809251394333065553493296640760748560200586941860545380978205674086221273349
@@ -56,6 +59,7 @@ impl ark_ff::FpParameters for FpParameters {
         0xFAEABB739ABD2280,
         0xFFFFFFFFFFFFFFFF,
         0x3FFFFFFFFFFFFFF,
+        0x0
     ]);
 
     //904625697166532776746648320380374280100293470930272690489102837043110636674
@@ -65,16 +69,23 @@ impl ark_ff::FpParameters for FpParameters {
         0xFD755DB9CD5E9140,
         0xFFFFFFFFFFFFFFFF,
         0x1FFFFFFFFFFFFFF,
+        0x0
     ]);
 
     //GENERATOR = 7
-    const GENERATOR: BigInteger = BigInteger([0x7, 0x0, 0x0, 0x0]);
+    const GENERATOR: BigInteger = BigInteger([
+        0x0000000000000000,
+        0xc13f6a264e843739,
+        0xe537f5b135039e5d,
+        0x0000000000000008,
+        0x0000000000000000
+    ]);
 
     const MODULUS_BITS: u32 = 256;
 
     const CAPACITY: u32 = Self::MODULUS_BITS - 1;
 
-    const REPR_SHAVE_BITS: u32 = 0;
+    const REPR_SHAVE_BITS: u32 = 64;
 
     const INV: u64 = 5408259542528602431;
 }
