8d54b2a30224d23cb1828dba14424d781c8f525d
Xiang Xie <xiexiangiscas@gmail.com>
2022-04-07 10:37:19 +0800

    deserialize_uncompressed failed

--- utils/src/curve/secp256k1/tests.rs
+++ utils/src/curve/secp256k1/tests.rs
@@ -1,4 +1,6 @@
 use ark_algebra_test_templates::fields::{field_test, primefield_test, sqrt_field_test};
+use ark_algebra_test_templates::{curves::*, groups::*};
+use ark_ec::AffineCurve;
 use ark_std::test_rng;
 use rand::Rng;
 
@@ -22,4 +24,28 @@ fn test_fr() {
     field_test(a, b);
     sqrt_field_test(a);
     primefield_test::<secp256k1::Fr>();
+}
+
+#[test]
+fn test_serialize_and_deserialize(){}
+
+#[test]
+fn test_secp256k1_projective_curve() {
+    curve_tests::<secp256k1::Projective>();
+    sw_tests::<secp256k1::Secp256k1Parameters>();
+}
+
+#[test]
+fn test_secp256k1_projective_group() {
+    let mut rng = test_rng();
+    let a: secp256k1::Projective = rng.gen();
+    let b: secp256k1::Projective = rng.gen();
+    group_test(a, b);
+}
+
+#[test]
+fn test_secp256k1_generator() {
+    let generator = secp256k1::Affine::prime_subgroup_generator();
+    assert!(generator.is_on_curve());
+    assert!(generator.is_in_correct_subgroup_assuming_on_curve());
 }
